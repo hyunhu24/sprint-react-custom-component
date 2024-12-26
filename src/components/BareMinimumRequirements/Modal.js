72813f7de6964058ec0bb996529bf7b6bc6a1512
HyunHu <hyunhu024@gmail.com>
2024-12-26 12:48:19 +0900

    modal error test

--- src/components/BareMinimumRequirements/Modal.js
+++ src/components/BareMinimumRequirements/Modal.js
@@ -1,49 +1,188 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 
-export const ModalContainer = styled.div`
+export const ModalContainer = styled.div
   // TODO : Modal을 구현하는데 전체적으로 필요한 CSS를 구현합니다.
-`;
+  `width: 100%;
+  height: 100%;
+  position: relative;`
+;
 
-export const ModalBackdrop = styled.div`
-  // TODO : Modal이 떴을 때의 배경을 깔아주는 CSS를 구현합니다.
-`;
-
-export const ModalBtn = styled.button`
-  background-color: var(--coz-purple-600);
+export const ModalBackdrop = styled.div.attrs((props) => ({
+  role: 'presentation',
+  'aria-hidden': 'true'
+}))
+  `width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #000;
+  opacity: 0.5;
+  z-index: 1;`
+;
+export const ModalBtn = styled.button
+ ` background-color: var(--coz-purple-600);
   text-decoration: none;
   border: none;
   padding: 20px;
   color: white;
   border-radius: 30px;
   cursor: grab;
-`;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);`
+;
+
+export const CloseBtn = styled.button
+`background-color: transparent;
+  text-decoration: none;
+  border: none;
+  color: #000;
+  cursor: pointer;
+  position: absolute;
+  top: 5px;
+  right: 10px;`
+;
 
 export const ModalView = styled.div.attrs((props) => ({
   // attrs 메소드를 이용해서 아래와 같이 div 엘리먼트에 속성을 추가할 수 있습니다.
   role: 'dialog',
-}))`
+}))
   // TODO : Modal창 CSS를 구현합니다.
-`;
+ ` display: ${(props) => props.$isActive ? 'flex' : 'none'};
+  width: 80%;
+  height: 20%;
+  background-color: white;
+  border-radius: 20px;
+  border: 1px solid #000;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  z-index: 20;
+  /* display: flex; */
+  align-items: center;
+  justify-content: center;`
+;
 
 export const Modal = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+
   const openModalHandler = () => {
     // TODO : isOpen의 상태를 변경하는 메소드를 구현합니다.
+    setIsOpen(!isOpen)
   };
+
+
+  // TODO : 클릭하면 Modal이 열린 상태(isOpen)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다.
 
   return (
     <>
-      <ModalContainer>
-        <ModalBtn
-        // TODO : 클릭하면 Modal이 열린 상태(isOpen)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다.
-        >
-          Open Modal
-          {/* TODO : 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때는 ModalBtn의 내부 텍스트가 'Opened!' 로 Modal이 닫힌 상태(isOpen이 false인 상태)일 때는 ModalBtn 의 내부 텍스트가 'Open Modal'이 되도록 구현해야 합니다. */}
-        </ModalBtn>
-        {/* TODO : 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때만 모달창과 배경이 뜰 수 있게 구현해야 합니다. */}
-      </ModalContainer>
+        <ModalBtn onClick={openModalHandler}>{isOpen ? 'Open Modal' : 'Opened'}</ModalBtn>
+        
+        {isOpen &&
+          <ModalContainer>
+          <ModalBackdrop onClick={openModalHandler} />
+          <ModalView $isActive={isOpen}>
+            <CloseBtn onClick={openModalHandler}>X</CloseBtn>
+            <p>Hello!!</p>
+          </ModalView>
+          </ModalContainer>}
     </>
   );
 };
+
+
+// import { useState } from 'react';
+// import styled from 'styled-components';
+
+// export const ModalContainer = styled.div`
+//   position: relative; // 부모
+//   height: 100%;
+// `;
+
+// export const ModalBackdrop = styled.div`
+//   position: absolute; // 자식
+//   background-color: rgba(0, 0, 0, 0.5);
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   top: 0;
+//   left: 0;
+//   right: 0;
+//   bottom: 0;
+// `;
+
+// export const ModalBtn = styled.button`
+//   background-color: var(--coz-purple-600);
+//   text-decoration: none;
+//   border: none;
+//   padding: 20px;
+//   color: white;
+//   border-radius: 30px;
+//   cursor: grab;
+//   position: absolute; // 자식
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+// `;
+
+// export const CloseBtn = styled.button`
+//   position: absolute;
+//   top: 10px;
+//   cursor: pointer;
+//   text-decoration: none;
+//   border: none;
+//   background-color: transparent;
+//   font: 15px;
+// `;
+
+// export const ModalView = styled.div.attrs((props) => ({
+//   // attrs 메소드를 이용해서 아래와 같이 div 엘리먼트에 속성을 추가할 수 있습니다.
+//   role: "dialog",
+// }))`
+// position: relative;
+//   width: 50%;
+//   height: 50%;
+//   background-color: white;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   padding: 10px;
+//   color: #442cdc;
+//   border-radius: 25px;
+// `;
+
+// export const Modal = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const openModalHandler = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <>
+//       <ModalContainer>
+//         <ModalBtn
+//           onClick={openModalHandler}
+//         >
+//           {isOpen ? "Opened!" : "Open Modal"}
+//         </ModalBtn>
+//         {isOpen ? (
+//           <>
+//             <ModalBackdrop onClick={openModalHandler}>
+//               <ModalView onClick={(event) => event.stopPropagation()}>
+//                 <CloseBtn onClick={openModalHandler}>x</CloseBtn>
+//                 HELLO CODESTATES!
+//               </ModalView>
+//             </ModalBackdrop>
+//           </>
+//         ) : null}
+//       </ModalContainer>
+//     </>
+//   );
+// };
--- src/components/BareMinimumRequirements/Toggle.js
+++ src/components/BareMinimumRequirements/Toggle.js
@@ -12,7 +12,12 @@ const ToggleContainer = styled.div`
     height: 24px;
     border-radius: 30px;
     background-color: #8b8b8b;
+    
     // TODO : .toggle--checked 클래스가 활성화 되었을 경우의 CSS를 구현합니다.
+    transition: all 0.8s ease-out;
+    &.toggle--checked {
+      background-color: #442cdc;
+    }
   }
 
   > .toggle-circle {
@@ -23,12 +28,22 @@ const ToggleContainer = styled.div`
     height: 22px;
     border-radius: 50%;
     background-color: #ffffff;
+    transition: all 0.5s ease-out;
     // TODO : .toggle--checked 클래스가 활성화 되었을 경우의 CSS를 구현합니다.
+    &.toggle--checked{
+      
+      left: 27px;
+    }
   }
 `;
 
 const Desc = styled.div`
   // TODO : 설명 부분의 CSS를 구현합니다.
+  text-align: center;
+  font-size: 13px;
+  /* border: 1px solid #000; */
+  padding: 10px;
+  font-weight: 600;
 `;
 
 export const Toggle = () => {
@@ -36,18 +51,21 @@ export const Toggle = () => {
 
   const toggleHandler = () => {
     // TODO : isOn의 상태를 변경하는 메소드를 구현합니다.
+    setisOn(!isOn);
   };
 
   return (
     <>
-      <ToggleContainer
+      <ToggleContainer onClick={toggleHandler}
         // TODO : 클릭하면 토글이 켜진 상태(isOn)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다.
       >
         {/* TODO : 아래에 div 엘리먼트 2개가 있습니다. 각각의 클래스를 'toggle-container', 'toggle-circle' 로 지정하세요. */}
         {/* TIP : Toggle Switch가 ON인 상태일 경우에만 toggle--checked 클래스를 div 엘리먼트 2개에 모두 추가합니다. 조건부 스타일링을 활용하세요. */}
-        <div />
-        <div />
+        <div className={`toggle-container ${isOn ? "toggle--checked" : ""}`}/>
+        <div className={`toggle-circle ${isOn ? "toggle--checked" : ""}`}/>
+        
       </ToggleContainer>
+      <Desc>Toggle Switch {isOn? "On" : "Off"}</Desc>
       {/* TODO : Desc 컴포넌트를 활용해야 합니다. */}
       {/* TIP:  Toggle Switch가 ON인 상태일 경우에 Desc 컴포넌트 내부의 텍스트를 'Toggle Switch ON'으로, 그렇지 않은 경우 'Toggle Switch OFF'가 됩니다. 조건부 렌더링을 활용하세요. */}
     </>
